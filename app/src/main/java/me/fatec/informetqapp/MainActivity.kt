bbfc4f1372cab2cda58016fc794afe83969bc2dd
AllanDevincolagit <profissional.allandevincola@gmail.com>
2022-09-24 15:13:03 -0300

    commit draw

--- app/src/main/java/me/fatec/informetqapp/MainActivity.kt
+++ app/src/main/java/me/fatec/informetqapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package me.fatec.informetqapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        entraConta.setOnClickListener{
+            btentraConta()
+        }
+    }
+
+    private fun btentraConta(){
+        val login = Intent(this,login::class.java)
+        startActivity(login)
     }
 }
---
+++ app/src/main/java/me/fatec/informetqapp/login.kt
@@ -0,0 +1,4 @@
+package me.fatec.informetqapp
+
+class login {
+}
